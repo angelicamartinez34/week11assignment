030e7ee81ca28622841cea72467080a986be9617
angelicamartinez34 <144196835+angelicamartinez34@users.noreply.github.com>
2023-12-07 17:06:10 -0700

    Add files via upload

---
+++ mysql-java/src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,38 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+
+	
+	private static String HOST = "localhost";
+	private static String PASSWORD = "projects";
+	private static int PORT = 3306;
+	private static String SCHEMA = "projects";
+	private static String USER = "projects";
+	
+	/**
+	 * @return
+	 */
+	public static Connection getConnection() {
+		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER, PASSWORD);
+		
+System.out.println(" Connecting with url" + uri);
+		
+		try {
+			Connection conn = DriverManager.getConnection(uri);
+			System.out.println("Connection succesful!");
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("error getting connection");
+			throw new DbException(e);
+			
+		}		
+	}
+}
+
+
